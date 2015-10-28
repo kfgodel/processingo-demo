ee957364cd36ab71c34c6ebe1b01bba7eb7677e3
Dario L Garcia <kfgodel@gmail.com>
2015-10-27 22:04:04 -0500

    Viewport setup

--- src/main/java/ar/com/kfgodel/demo/DemoHelloWorld.java
+++ src/main/java/ar/com/kfgodel/demo/DemoHelloWorld.java
@@ -2,17 +2,27 @@ package ar.com.kfgodel.demo;
 
 import ar.com.kfgodel.processingo.api.ProcessingRunner;
 import ar.com.kfgodel.processingo.api.ProcessingSketch;
+import ar.com.kfgodel.processingo.api.original.ProcessingConfiguration;
+import ar.com.kfgodel.processingo.api.original.ProcessingRenderer;
+import ar.com.kfgodel.processingo.api.viewports.ViewSize;
+import ar.com.kfgodel.processingo.api.viewports.ViewportDefinition;
 import ar.com.kfgodel.processingo.impl.PappletRunner;
 import ar.com.kfgodel.processingo.impl.SketchSupport;
 
 /**
- * This is the most basic example showing a hello world text in a small window
+ * This is the a basic example showing a hello world text in a small window
  * Created by tenpines on 26/10/15.
  */
 public class DemoHelloWorld {
 
   public static void main(String[] args) {
-    ProcessingSketch sketch = new SketchSupport(){};
+    ProcessingSketch sketch = new SketchSupport(){
+      @Override
+      public void onSettings(ProcessingConfiguration configuration) {
+        ViewportDefinition.window(ViewSize.of(200, 100), ProcessingRenderer.P2D)
+          .change(configuration);
+      }
+    };
     ProcessingRunner runner = PappletRunner.create();
     runner.run(sketch);
   }
---
+++ src/main/java/ar/com/kfgodel/demo/DemoSimplestSketch.java
@@ -0,0 +1,20 @@
+package ar.com.kfgodel.demo;
+
+import ar.com.kfgodel.processingo.api.ProcessingRunner;
+import ar.com.kfgodel.processingo.api.ProcessingSketch;
+import ar.com.kfgodel.processingo.impl.PappletRunner;
+import ar.com.kfgodel.processingo.impl.SketchSupport;
+
+/**
+ * This is the most basic example showing an empty sketch behavior
+ * Created by tenpines on 26/10/15.
+ */
+public class DemoSimplestSketch {
+
+  public static void main(String[] args) {
+    ProcessingSketch sketch = new SketchSupport();
+    ProcessingRunner runner = PappletRunner.create();
+    runner.run(sketch);
+  }
+
+}
