6891ae9ee52b876e6ca772dab040150292a07cdc
Dario L Garcia <kfgodel@gmail.com>
2015-11-01 20:10:20 -0600

    Add worker thread

--- src/main/java/ar/com/kfgodel/demo/DemoBouncingBall.java
+++ src/main/java/ar/com/kfgodel/demo/DemoBouncingBall.java
@@ -4,12 +4,17 @@ import ar.com.kfgodel.demo.bounce.BouncingBall;
 import ar.com.kfgodel.processingo.api.ProcessingSketch;
 import ar.com.kfgodel.processingo.api.original.ProcessingRenderer;
 import ar.com.kfgodel.processingo.api.space.Vector2d;
+import ar.com.kfgodel.processingo.api.time.TimeQuantity;
 import ar.com.kfgodel.processingo.api.viewports.ViewSize;
 import ar.com.kfgodel.processingo.api.viewports.ViewportDefinition;
 import ar.com.kfgodel.processingo.impl.PappletRunner;
 import ar.com.kfgodel.processingo.impl.sketchs.DescribeWorldPerFrameSketch;
 import ar.com.kfgodel.processingo.impl.visuals.BackgroundVisual;
 import ar.com.kfgodel.processingo.impl.visuals.EllipseVisual;
+import ar.com.kfgodel.processingo.worker.api.WorkerTask;
+import ar.com.kfgodel.processingo.worker.api.WorkerThread;
+
+import java.util.concurrent.TimeUnit;
 
 /**
  * This demo shows a bouncing ball
@@ -19,6 +24,10 @@ public class DemoBouncingBall {
 
   public static void main(String[] args) {
     BouncingBall ball = BouncingBall.createDefault();
+
+    WorkerThread.create()
+      .execute(WorkerTask.periodicWith(TimeQuantity.of(500, TimeUnit.MILLISECONDS), ball::move));
+
     ProcessingSketch sketch = DescribeWorldPerFrameSketch
       .create(ViewportDefinition.window(ViewSize.of(640, 480), ProcessingRenderer.P2D),
         (canvas)-> {
@@ -26,7 +35,6 @@ public class DemoBouncingBall {
             .applyOn(canvas);
           EllipseVisual.create(ball.position().product(Vector2d.xy(640, 480)), ball.radius() * 640, ball.radius() * 480)
             .applyOn(canvas);
-          ball.move();
         });
     PappletRunner.create().run(sketch);
   }
